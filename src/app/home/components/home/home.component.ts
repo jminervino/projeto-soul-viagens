3c4befe9ac0066021defd344c1bd870a7734192e
jminervino <joaovictor_minervino@hotmail.com>
2022-06-15 08:54:05 -0300

    Edição da homepage

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -10,6 +10,7 @@ export class HomeComponent implements OnInit {
   bgImg = "pexels-_getinspoco_-bibhash-banerjee-1056528.jpg"
   
   imgCarousel: String[] = ["./assets/img/img-carousel01.jpg", "./assets/img/img-carousel02.jpg", "./assets/img/img-carousel03.jpg"]
+  imgCarousel2: String[] = ["./assets/img/img-carousel04.jpg", "./assets/img/img-carousel05.jpg", "./assets/img/img-carousel06.jpg"]
   
   constructor() { }
 
